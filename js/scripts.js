46890f56e1caf85eca96e236207ad8978aaa34b3
alexandre-silvaa <alexandrebsilva9@gmail.com>
2022-12-10 21:40:41 -0300

    To Do List Finalizado versao0.1

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,8 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEdit = document.querySelector("#cancel-edit-btn");
 
+let oldInput;
+
 //funções
 const saveTodo = (text) => {
 	const todo = document.createElement("div");
@@ -39,6 +41,31 @@ const saveTodo = (text) => {
 	todo.appendChild(deleteBtn);
 
 	todoList.appendChild(todo);
+
+	todoInput.value = "";
+	todoInput.focus();
+}
+
+const toggleForms = () => {
+	editForm.classList.toggle("hide");
+	todoForm.classList.toggle("hide");
+	todoList.classList.toggle("hide");
+} 
+
+const updateTodo = (text) => {
+
+	const todos = document.querySelectorAll(".todo");
+
+	todos.forEach((todo) => {
+
+		let todoTitle = todo.querySelector("h3");
+
+		if(todoTitle.innerText === oldInput){
+			todoTitle.innerText = text;
+		}
+
+	})
+
 }
 
 //eventos
@@ -50,4 +77,48 @@ todoForm.addEventListener("submit", (e) => {
 	if(inputValue) {
 		saveTodo(inputValue);
 	}
+});
+
+document.addEventListener("click", (e) => {
+
+	const targetEl = e.target;
+	const parentEl = targetEl.closest("div");
+	let todoTitle;
+
+	if(parentEl && parentEl.querySelector("h3")){
+		todoTitle = parentEl.querySelector("h3").innerText;
+	}
+
+	if(targetEl.classList.contains("finish-todo")){
+		parentEl.classList.toggle("done");
+	}
+
+	else if(targetEl.classList.contains("remove-todo")){
+		parentEl.remove();
+	}
+
+	else if(targetEl.classList.contains("edit-todo")){
+		toggleForms();
+
+		editInput.value = todoTitle;
+		oldInput = todoTitle;
+	}
+});
+
+cancelEdit.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	const editInputValue = editInput.value;
+
+	if(editInputValue){
+		updateTodo(editInputValue);
+	}
+
+	toggleForms();
 });
